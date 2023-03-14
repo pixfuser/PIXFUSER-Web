6b1edc1cdd6f0371bfcafc8476b97bbdf14232da
Akash Kumar Singh <dipu64601@gmail.com>
2023-03-14 14:27:32 +0530

    Improved navigation responsiveness

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./CSS/navigation.css";
 import logo from "./Images/logo.png";
+import menu from "./Images/icons/menu.png";
+import close from "./Images/icons/close.png";
 
 function Navigation() {
+  useEffect(() => {
+    const menuIco = document.getElementById("menu");
+    const closeIco = document.getElementById("close");
+    const menus = document.querySelector("#nav_options div");
+    menuIco.addEventListener("click", () => {
+      menus.style.top = "80px";
+      setTimeout(() => {
+        document.addEventListener("click", hidePopup);
+        menuIco.style.display = "none";
+        closeIco.style.display = "inline-block";
+      }, 50);
+    });
+
+    function hidePopup() {
+      menus.style.top = "-110px";
+      menuIco.style.display = "inline-block";
+      closeIco.style.display = "none";
+
+      document.removeEventListener("click", hidePopup);
+    }
+  }, []);
+
   function scrollScreen(val) {
     window.scrollTo({
       top: window.innerHeight * (val - 0.1),
@@ -45,6 +69,8 @@ function Navigation() {
           </span>
         </div>
         <button className="get_card">Get Your Card</button>
+        <img src={menu} alt="menu" id="menu" />
+        <img src={close} alt="close" id="close" />
       </div>
     </nav>
   );
